60233dd29bc179929c59a9eb9e0d93b5f935043c
Goran Kopevski <gkopevski@gmail.com>
2017-02-21 22:46:16 +0100

    added CORS exception

--- src/login/handler.js
+++ src/login/handler.js
@@ -7,6 +7,12 @@ module.exports.login = (event, context, callback) => {
   }
   const response = {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods':'GET,OPTIONS',
+    },
     body: JSON.stringify({
       message: 'Good login',
       allowed: allowed,
--- src/results/post.js
+++ src/results/post.js
@@ -11,6 +11,12 @@ module.exports.users = (event, context, callback) => {
   let user = JSON.parse(event.body);
   let response = {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods':'GET,OPTIONS',
+    },
     body: {},
   };
   let tableName = "users";
